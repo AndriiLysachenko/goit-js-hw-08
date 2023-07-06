e3cfbefa24b964f5b0a4378c2b44e5fd1340fa4b
AndriiLysachenko <andreylysachenko38@gmail.com>
2023-07-06 17:03:13 +0330

    task2 new_one

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,16 @@
 import Player from '@vimeo/player';
-
+import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-import throttle from "lodash.throttle";
-
-const timeVideo = JSON.parse(localStorage.getItem("videoplayer-current-time")) || {};
-const seconds = timeVideo.seconds;
-console.log(seconds);
 
 
-player.on('timeupdate', throttle(function(data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+const seconds = localStorage.getItem("videoplayer-current-time") || 0;
+
+
+
+player.on('timeupdate', throttle(function({seconds}) {
+    localStorage.setItem("videoplayer-current-time", seconds);
 }, 1000));
 
 
-player.setCurrentTime(seconds).then(function() {
-    
-}).catch(function(error) {
-    switch (error.name) {
-
-        case 'RangeError':
-            break;
-
-        default:
-            break;
-    }
-});
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-player.on("play", player.setCurrentTime);
+player.setCurrentTime(seconds);
