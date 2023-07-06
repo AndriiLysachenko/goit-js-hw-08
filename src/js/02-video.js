9ee222b93cbadfad9967defbbaaffacf52cfb55f
AndriiLysachenko <andreylysachenko38@gmail.com>
2023-07-06 12:05:33 +0330

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,49 +1,33 @@
-
-//import Player from '@vimeo/player';
-
-//const iframe = document.querySelector('iframe');
-//const player = new Player(iframe);
-//import throttle from "lodash.throttle";
-
-//const timeVideo = JSON.parse(localStorage.getItem("videoplayer-current-time")) || {};
-//const seconds = timeVideo.seconds;
-//console.log(seconds);
-
-
-//player.on('timeupdate', throttle(function(seconds) {
-    //localStorage.setItem("videoplayer-current-time", JSON.stringify(//seconds));
-//}, 1000));
-
-
-//player.setCurrentTime(seconds).then(function() {
-    
-//}).catch(function(error) {
-   // switch (error.name) {
-
-    //    case 'RangeError':
-    //        break;
-
-    //    default:
-    //        break;
-  //  }
-//});
-
-//player.getVideoTitle().then(function(title) {
-  //  console.log('title:', title);
-//});
-//player.on("play", player.setCurrentTime);
-
-import player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+import throttle from "lodash.throttle";
 
-const onTimeupdate = function (event) { 
-    console.log(event);
-    localStorage.setItem("videoplayer-current-time", event.seconds)
-};
+const timeVideo = JSON.parse(localStorage.getItem("videoplayer-current-time")) || {};
+const seconds = timeVideo.seconds;
+console.log(seconds);
+
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+}, 1000));
+
+
+player.setCurrentTime(seconds).then(function() {
     
-player.on('timeupdate', throttle(onTimeupdate, 1000));
-//player.off('timeupdate', onTimeupdate);
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
+}).catch(function(error) {
+    switch (error.name) {
+
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+player.on("play", player.setCurrentTime);
